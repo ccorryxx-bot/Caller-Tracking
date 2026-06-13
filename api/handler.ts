6a9423ded30e36d21bc39542730bd519b23b5df4
KYAW GYII - YFM <ccorryxx@gmail.com>
2026-06-13 19:17:18 +0630

    debug: binary search - routers only

--- api/handler.ts
+++ api/handler.ts
@@ -1,31 +1,23 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import express from "express";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { appRouter } from "../server/routers";
+import { createContext } from "../server/_core/context";
 
 let app: any = null;
 let initErr: any = null;
 
-function init() {
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const express = require("express");
-    const { createExpressMiddleware } = require("@trpc/server/adapters/express");
-    const { registerOAuthRoutes } = require("../server/_core/oauth");
-    const { registerStorageProxy } = require("../server/_core/storageProxy");
-    const { appRouter } = require("../server/routers");
-    const { createContext } = require("../server/_core/context");
-
-    const a = express();
-    a.use(express.json({ limit: "50mb" }));
-    a.use(express.urlencoded({ limit: "50mb", extended: true }));
-    registerStorageProxy(a);
-    registerOAuthRoutes(a);
-    a.use("/api/trpc", createExpressMiddleware({ router: appRouter, createContext }));
-    app = a;
-  } catch (e: any) {
-    initErr = e;
-  }
+try {
+  const a = express();
+  a.use(express.json({ limit: "50mb" }));
+  a.use(
+    "/api/trpc",
+    createExpressMiddleware({ router: appRouter, createContext })
+  );
+  app = a;
+} catch (e: any) {
+  initErr = e;
 }
-
-init();
 
 export default (req: VercelRequest, res: VercelResponse) => {
   if (initErr || !app) {
